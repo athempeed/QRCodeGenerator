4adc2131634830eec5297969f6b3996ea158fd39
donjay2101 <mehta.deepak@outlook.com>
2021-10-05 19:54:17 +0800

    initial commit

---
+++ Function1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using QRCoder;
+
+namespace QRGenerator
+{
+    public static class Function1
+    {
+        [FunctionName("QRGenerator")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string text = req.Query["text"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+           
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return new BadRequestResult();
+            }
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                var qrCode = new PngByteQRCode(qrCodeData);
+                var qrCodeAsPng = qrCode.GetGraphic(20);
+                return new FileContentResult(qrCodeAsPng, "image/png");
+            }
+        }
+    }
+}
